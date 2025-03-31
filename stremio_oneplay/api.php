12d0dbe24e81332cbf70ba5430f770a71bc52e38
Flix Git <130175336+flixtx@users.noreply.github.com>
2025-03-31 08:38:52 -0300

    Update api.php

--- stremio_oneplay/api.php
+++ stremio_oneplay/api.php
@@ -3,7 +3,7 @@
 function tvshows($imdb, $season, $episode) {
     $stream = '';
     //$base = 'https://superflixapi.dev';
-    $base = 'https://superflixapi.ps';
+    $base = 'https://superflixapi.cc';
     $headers = [
         'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.1 Safari/537.36'
     ];
